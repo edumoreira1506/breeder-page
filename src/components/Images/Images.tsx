55a5ada8a216cf66802721f4e60bf5de05354d25
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-11 17:44:51 -0300

    Add image gallery

--- src/components/Images/Images.tsx
+++ src/components/Images/Images.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react'
-import { ImagesCarousel } from '@cig-platform/ui'
+import React, { FC, useState, useCallback } from 'react'
+import Modal from 'react-modal'
+import { ImagesCarousel, ImageGallery, Colors } from '@cig-platform/ui'
 
 import { StyledContainer, StyledTitle } from './Images.styles'
 
@@ -11,11 +12,43 @@ export interface ImagesProps {
 }
 
 const Images: FC<ImagesProps> = ({ images }: ImagesProps) => {
+  const [isOpenGallery, setIsOpenGallery] = useState(false)
+  const [clickedIndex, setClickedIndex] = useState(0)
+
+  const closeGallery = useCallback(() => setIsOpenGallery(false), [])
+  const openGallery = useCallback(() => setIsOpenGallery(true), [])
+
+  const handleClickImage = useCallback((src: string) => {
+    console.log({ src })
+
+    openGallery()
+
+    const index = images.findIndex((image) => image.src === src)
+
+    setClickedIndex(index)
+  }, [openGallery, images])
+
   return (
-    <StyledContainer>
-      <StyledTitle>Fotos do criatório</StyledTitle>
-      <ImagesCarousel images={images} onClickImage={() => undefined} />
-    </StyledContainer>
+    <>
+      <StyledContainer>
+        <StyledTitle>Fotos do criatório</StyledTitle>
+        <ImagesCarousel images={images} onClickImage={handleClickImage} />
+      </StyledContainer>
+      <Modal
+        isOpen={isOpenGallery}
+        onRequestClose={closeGallery}
+        style={{ overlay: { background: Colors.BlackTransparent, zIndex: 1000 } }}
+      >
+        <ImageGallery
+          showPlayButton={false}
+          items={images.map((image) => ({
+            original: image.src,
+            thumbnail: image.src
+          }))}
+          startIndex={clickedIndex}
+        />
+      </Modal>
+    </>
   )
 }
 
