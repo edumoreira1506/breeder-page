2053b503a82787704c0c69be43485d8dabe3cadf
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-17 23:38:13 -0300

    Change video styles (#6)

--- src/components/Video/Video.styles.ts
+++ src/components/Video/Video.styles.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components'
-import { createMinWidthMediaQuery } from '@cig-platform/ui'
+import { Colors, createMinWidthMediaQuery, DEFAULT_BORDER_RADIUS } from '@cig-platform/ui'
 
 export const StyledContainer = styled.div`
-  width: 100%;
-  height: 170px;
+  height: 150px;
+  background-color: ${Colors.Black};
+  padding: 10px;
+  border-radius: ${DEFAULT_BORDER_RADIUS};
+  width: 85%;
+  margin: 0 auto;
 
   ${createMinWidthMediaQuery(`
-    width: 690px;
+    width: 600px;
     height: 400px;
     margin: 0 auto;
   `)}
@@ -14,5 +18,7 @@ export const StyledContainer = styled.div`
   & > div {
     width: 100% !important;
     height: 100% !important;
+    overflow: hidden;
+    border-radius: ${DEFAULT_BORDER_RADIUS};
   }
 `
