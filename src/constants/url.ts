65a673ecf8053e3b3e0be0487d6d4d0e3eaef3ca
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-23 23:09:24 -0300

    Replace poultries for micro front end (#16)
    * Replace poultries for micro front end
    
    * Add env variable

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,2 +1,3 @@
 export const CONTENT_SEARCH_API_URL = String(process.env.REACT_APP_CONTENT_SEARCH_API_URL)
 export const MARKETPLACE_URL = String(process.env.REACT_APP_MARKETPLACE_URL)
+export const POULTRY_CAROUSELS_PAGE_URL = String(process.env.REACT_APP_POULTRY_CAROUSELS_PAGE_URL)
